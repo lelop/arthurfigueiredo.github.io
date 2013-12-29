5f30dcdd1b7e1ec3d6c00b9d3120d19df23a9ef9
Arthur Figueiredo <=>
2013-12-29 02:14:14 -0200

    Gh pages commit

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Arthur Figueiredo - Web Developer</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" type="image/ico" href="img/favicon.ico">
+	<link rel="stylesheet" href="css/screen.css">
+
+
+	<!--[if lt IE 9]>
+		<link href="css/ie.css" rel="stylesheet" />
+	<![endif]-->
+
+</head>
+<body>
+	<!-- BEGIN HEADER -->
+	<header>
+		<div class="headercontent">
+			<div class="container title caps">
+				<h1 class="fadeInUp">Arthur Figueiredo</h1>
+				<h2 class="fadeInUp">Front End &amp; Back End Engineer</h2>
+			</div>
+			<div class="container social fadeInUp">
+				<div class="sideline fadeInLeft"></div>
+				<a href="#" target="_blank"><img src="img/twitter.png" height="44" width="44"></a>
+				<a href="#" target="_blank"><img src="img/dribbble.png" height="44" width="44"></a>
+				<a href="#" target="_blank"><img src="img/linkedin.png" height="44" width="44"></a>
+				<div class="sideline fadeInRight"></div>
+			</div>
+			<div class="container">
+				<div class="detail down-arrow fadeInDown"><h1 id="go-down">Scroll down</h1></div>
+			</div>
+		</div>
+	</header>
+	<!-- END HEADER -->
+
+	<!-- BEGIN CONTENT -->
+	<section id="content">
+
+		<!-- BEGIN WORKS -->
+		<div class="container workcontent">
+			<div class="container project pie-clearfix">
+				<div class="preview pleft trans">
+					<img class="pimgb" src="projects/kolina-chevrolet.png">
+				</div>
+				<div class="info iright trans">
+					<h2>Kolina Chevrolet</h2>
+					<h3><span class="role">tags:</span> visual &amp; interaction design</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, ut a architecto incidunt dicta molestiae nostrum ducimus doloremque cum unde.</p>
+					<a href="http://node1.#" class="button caps">View Project</a>
+				</div>
+			</div>
+
+			<div class="container project pie-clearfix">
+				<div class="preview pright trans">
+					<img class="pimgb" src="projects/petfeliz.png">
+				</div>
+				<div class="info ileft trans">
+					<h2>PetFeliz</h2>
+					<h3><span class="role">tags:</span> visual design, art direction</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure cum amet quibusdam voluptate incidunt.</p>
+					<a href="#" class="button caps">View Project</a>
+				</div>
+			</div>
+			
+			<div class="container project pie-clearfix">
+				<div class="preview pleft trans">
+					<img class="pimgb" src="projects/zeza.png">
+				</div>
+				<div class="info iright trans">
+					<h2>ADMT Basic Template</h2>
+					<h3><span class="role">tags:</span> visual &amp; interaction design</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, quis aspernatur dolore sed quo placeat reprehenderit voluptas aliquid incidunt.</p>
+					<a href="http://node1.#" class="button caps">View Project</a>
+				</div>
+			</div>
+			
+			<div class="container project pie-clearfix">
+				<div class="preview pright trans">
+					<img class="pimgb" src="projects/delupo.png">
+				</div>
+				<div class="info ileft trans">
+					<h2>Delupo eCommerce</h2>
+					<h3><span class="role">tags:</span> visual design, art direction</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, qui, sint dicta repellendus quia quae ex nisi.</p>
+					<a href="#" class="button caps">View Project</a>
+				</div>
+			</div>
+
+
+
+		</div>
+		<!-- END WORKS -->
+		
+		<!-- BEGIN ABOUT -->
+		<div class="aboutcontent">
+			<div class="container about">
+				<div class="bio">
+					
+					<h1>Passionate about web mobile development and advocate of open source initiatives.</h1>
+
+ 					<p>I'm Arthur Figueiredo, I'm only 18 years old, working with web development for over two years, currently working on ADMT, where I'm responsible for the front-end development and development of virtual stores using the Magento platform.</p>	
+				</div>
+			</div>
+		</div>
+		<!-- END ABOUT -->
+
+		<!-- BEGIN CONTACT -->
+		<div class="contactcontent">
+			<div class="container contact">
+				<h3>For Web Development or OpenSource projects<br>Get In Touch</h3>
+				<h4><a href="mailto:hi@arthurfigueiredo.com.br">arthurfn.webdev@gmail.com</a></h4>
+			</div>
+		</div>
+
+		<!-- BEGIN FOOTER -->
+		<footer>
+			<p>Thank you for watching :)</p>
+		</footer>
+		<!-- END FOOTER -->
+
+	</section>
+	
+	<script src="js/jquery.js"></script>
+	<script src="js/main.js"></script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-40576737-1', 'arthurfigueiredo.com.br');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
