f7d5e5a9e1ebe3e5f672ce154341548d80b93b3f
Arthur Figueiredo <=>
2013-12-29 00:34:42 -0200

    First Commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var projects = document.querySelectorAll(".preview,.info");
+var projectsStartsAfter=780;
+var body = document.body;
+$(function(){
+
+	window.addEventListener("scroll",function(){
+		
+		var n=Math.max($("body").scrollTop(),$("html").scrollTop());
+		
+		var i = projects.length;
+		while(i--){
+			n+$(window).height()-projectsStartsAfter>$(projects[i]).position().top ? $(projects[i]).classList.add("trans"): projects[i].classList.remove("trans");
+		}
+
+	});
+
+});
